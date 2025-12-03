6b3e95671b3e0659901ffd594f5ded2f0d203270
bytherusso <bytherusso@gmail.com>
2025-12-03 10:13:42 -0600

    "Subiendo web final"

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ProyectoPro_React/", // Tu nombre de repo
+  base: "/ProyectoPro_React/", 
   build: {
-    outDir: 'docs', // <--- ESTO ES LO NUEVO
+    outDir: '../docs', // <--- OJO AQUÍ: Los dos puntos .. sacan la carpeta afuera
   }
 })
